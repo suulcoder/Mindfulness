f0f6a4aa268b4c0616593fa4f667fc5962dd87a8
suulcoder <ef.proyectomundial@gmail.com>
2020-11-21 22:11:57 -0600

    Updates

---
+++ precache-manifest.e94dbcbe571c6e2302fa2c3260b8fa8d.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fd1cfd5cd0df9185ea56b73cb1748f2",
+    "url": "/mindfulness/index.html"
+  },
+  {
+    "revision": "029da8ed88a8099123da",
+    "url": "/mindfulness/static/css/main.d1f10c02.chunk.css"
+  },
+  {
+    "revision": "e843923ca79e67434000",
+    "url": "/mindfulness/static/js/2.7cab1873.chunk.js"
+  },
+  {
+    "revision": "88c0bfd11a2be5419414edbddb842b0f",
+    "url": "/mindfulness/static/js/2.7cab1873.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "029da8ed88a8099123da",
+    "url": "/mindfulness/static/js/main.1c4c34ca.chunk.js"
+  },
+  {
+    "revision": "98cbdfdd61815a39cf4a",
+    "url": "/mindfulness/static/js/runtime-main.e2a5683e.js"
+  },
+  {
+    "revision": "d42a3d0bc55026239aaead2fa1b91986",
+    "url": "/mindfulness/static/media/1.d42a3d0b.png"
+  },
+  {
+    "revision": "2c83300c9a8a6f3933cbd07b611e7377",
+    "url": "/mindfulness/static/media/10.2c83300c.png"
+  },
+  {
+    "revision": "6b5824acde912d366e0bc0a3c327219a",
+    "url": "/mindfulness/static/media/11.6b5824ac.png"
+  },
+  {
+    "revision": "d9199cbe7cb91bd319a665af95503606",
+    "url": "/mindfulness/static/media/12.d9199cbe.png"
+  },
+  {
+    "revision": "2e25a87f2fa732a6a2f3480befc141b3",
+    "url": "/mindfulness/static/media/13.2e25a87f.png"
+  },
+  {
+    "revision": "256e9dc2c6056d2a0e4eaa6934cdb0d2",
+    "url": "/mindfulness/static/media/2.256e9dc2.png"
+  },
+  {
+    "revision": "fce7b82b69d8fc50473833311ed8298f",
+    "url": "/mindfulness/static/media/3.fce7b82b.png"
+  },
+  {
+    "revision": "0b56035d4c27ce553a6c6e7d24d2b2fc",
+    "url": "/mindfulness/static/media/4.0b56035d.png"
+  },
+  {
+    "revision": "449b247dbe25eae7d388d8b1adab5ea0",
+    "url": "/mindfulness/static/media/5.449b247d.png"
+  },
+  {
+    "revision": "cde35bac0fdc3af8326482bdeae99031",
+    "url": "/mindfulness/static/media/6.cde35bac.png"
+  },
+  {
+    "revision": "7cbce2bfe6f8ed7166b64d0d05f46dfd",
+    "url": "/mindfulness/static/media/7.7cbce2bf.png"
+  },
+  {
+    "revision": "818a5c4f349e0f7304524519a0859c87",
+    "url": "/mindfulness/static/media/8.818a5c4f.png"
+  },
+  {
+    "revision": "f2f085c25a0a5148683e1f2483124f18",
+    "url": "/mindfulness/static/media/9.f2f085c2.png"
+  },
+  {
+    "revision": "665d4d9cf944ad1a58c6da0d7fae4c2d",
+    "url": "/mindfulness/static/media/australia.665d4d9c.png"
+  },
+  {
+    "revision": "0592300301edd5a79a8eee7fe726915f",
+    "url": "/mindfulness/static/media/brain.05923003.png"
+  },
+  {
+    "revision": "5cb411c3c8c4b3d20beb3cdecaf25174",
+    "url": "/mindfulness/static/media/california.5cb411c3.png"
+  },
+  {
+    "revision": "75ca48cf1b3673711347452fdc5445ed",
+    "url": "/mindfulness/static/media/code.75ca48cf.jpg"
+  },
+  {
+    "revision": "f2281ec4611163fb5be55df91e5b2625",
+    "url": "/mindfulness/static/media/dsn.f2281ec4.png"
+  },
+  {
+    "revision": "54231c0670e8902d0d828b27f9f54904",
+    "url": "/mindfulness/static/media/earth.54231c06.png"
+  },
+  {
+    "revision": "ccf029864d48956464a0b1b08c5a4c8c",
+    "url": "/mindfulness/static/media/installation.ccf02986.jpg"
+  },
+  {
+    "revision": "91550fa798de74a3557872a7beaca5e2",
+    "url": "/mindfulness/static/media/lim.91550fa7.png"
+  },
+  {
+    "revision": "f3ad07d94352eaa89d9fbd0d40d7165c",
+    "url": "/mindfulness/static/media/lim2.f3ad07d9.jpg"
+  },
+  {
+    "revision": "39209afe367e126fca6c10894d0ea024",
+    "url": "/mindfulness/static/media/logo.39209afe.png"
+  },
+  {
+    "revision": "e7b79d609f57e60412ed20223aae6717",
+    "url": "/mindfulness/static/media/mind2.e7b79d60.jpg"
+  },
+  {
+    "revision": "40356c4834b0344292969a48e05a0f35",
+    "url": "/mindfulness/static/media/mind3.40356c48.jpg"
+  },
+  {
+    "revision": "5dae729ea7df123e75f5235451a2b445",
+    "url": "/mindfulness/static/media/satellite.5dae729e.png"
+  },
+  {
+    "revision": "57124beb118f70041dc6201bd5360faa",
+    "url": "/mindfulness/static/media/spain.57124beb.png"
+  },
+  {
+    "revision": "f8783a4e5064a52a6c6051c2b4886d1d",
+    "url": "/mindfulness/static/media/zapato.f8783a4e.jpg"
+  }
+]);
